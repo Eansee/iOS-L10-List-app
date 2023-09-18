24fbeaa8ccc662609c29f5228a17dbff30602621
Eansee <koh_ean_see@s2023.ssts.edu.sg>
2023-09-18 17:09:31 +0800

    Initial Commit

---
+++ iOS Lesson 10 Github/iOS_Lesson_10_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Lesson_10_GithubApp.swift
+//  iOS Lesson 10 Github
+//
+//  Created by Koh Ean See on 18/9/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Lesson_10_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
